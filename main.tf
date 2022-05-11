01fff7da71700ffdc289783398f6370b80b8ff52
Corentin Leclerc <corentin.leclerc@accenture.com>
2022-05-11 18:13:00 +0200

    🚀 : V1 of the lab w/ instructions

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "google_service_account" "sa-demo" {
+  count        = var.instance_count
+  account_id   = "sa-demo${count.index}"
+  display_name = "Service Account pour test de demo"
+}
+
+resource "google_compute_instance" "instance" {
+  count        = var.instance_count
+  name         = "${var.instance_name}${count.index}"
+  machine_type = "f1-micro"
+  zone         = var.zone_name
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    #maybe you don't have a default network on your projet.
+    #in this case use for example instead: subnet_name  = "projects/MYPROJECT/regions/MYREGION/subnetworks/MYSUBNETNAME"
+    network = "default"
+
+    #remove that if you don't want public IP for your instance
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.sa-demo[count.index].email
+    scopes = ["cloud-platform"]
+  }
+}
