27709e270a8f02ada5e29e4c2d0929f44d710267
Corentin Leclerc <corentin.leclerc@accenture.com>
2022-05-12 16:57:48 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "internal_instance_ip_addr" {
-  value = google_compute_instance.instance01.network_interface.0.network_ip
+  value = google_compute_instance.instance.*.network_interface.0.network_ip
 }
